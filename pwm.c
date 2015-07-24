bc12cfcfdc416451b9619b2c7421f8b4eea58992
Viresh Kumar <viresh.kumar@linaro.org>
2015-07-24 12:22:24 +0530

    gbsim: pwm: Start using send_response()
    Lets start using send_response() for pwm protocol drivers. This gets
    rid of lots of redundant code.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- pwm.c
+++ pwm.c
@@ -33,11 +33,7 @@ int pwm_handler(uint16_t cport_id, uint16_t hd_cport_id, void *rbuf,
 	__u32 period;
 	size_t payload_size;
 	uint16_t message_size;
-	uint8_t module_id;
 	uint8_t result = PROTOCOL_STATUS_SUCCESS;
-	ssize_t nbytes;
-
-	module_id = cport_to_module_id(cport_id);
 
 	op_rsp = (struct op_msg *)tbuf;
 	oph = (struct op_header *)&op_req->header;
@@ -47,24 +43,20 @@ int pwm_handler(uint16_t cport_id, uint16_t hd_cport_id, void *rbuf,
 		payload_size = sizeof(struct gb_protocol_version_response);
 		op_rsp->pv_rsp.major = GREYBUS_VERSION_MAJOR;
 		op_rsp->pv_rsp.minor = GREYBUS_VERSION_MINOR;
-		gbsim_debug("Module %hhu -> AP CPort %hu PWM protocol version response\n  ",
-			    module_id, cport_id);
 		break;
 	case GB_PWM_TYPE_PWM_COUNT:
 		payload_size = sizeof(struct gb_pwm_count_response);
 		op_rsp->pwm_cnt_rsp.count = 1; /* Something arbitrary, but useful */
-		gbsim_debug("Module %hhu -> AP CPort %hu PWM count response\n  ",
-			    module_id, cport_id);
 		break;
 	case GB_PWM_TYPE_ACTIVATE:
 		payload_size = 0;
-		gbsim_debug("AP -> Module %hhu CPort %hu PWM %d activate request\n  ",
-			    module_id, cport_id, op_req->pwm_act_req.which);
+		gbsim_debug("PWM %d activate request\n  ",
+			    op_req->pwm_act_req.which);
 		break;
 	case GB_PWM_TYPE_DEACTIVATE:
 		payload_size = 0;
-		gbsim_debug("AP -> Module %hhu CPort %hu PWM %d deactivate request\n  ",
-			    module_id, cport_id, op_req->pwm_deact_req.which);
+		gbsim_debug("PWM %d deactivate request\n  ",
+			    op_req->pwm_deact_req.which);
 		break;
 	case GB_PWM_TYPE_CONFIG:
 		payload_size = 0;
@@ -74,8 +66,8 @@ int pwm_handler(uint16_t cport_id, uint16_t hd_cport_id, void *rbuf,
 			libsoc_pwm_set_duty_cycle(pwms[op_req->pwm_cfg_req.which], duty);
 			libsoc_pwm_set_period(pwms[op_req->pwm_cfg_req.which], period);
 		}
-		gbsim_debug("AP -> Module %hhu CPort %hu PWM %d config (%dns/%dns) request\n  ",
-			    module_id, cport_id, op_req->pwm_cfg_req.which, duty, period);
+		gbsim_debug("PWM %d config (%dns/%dns) request\n  ",
+			    op_req->pwm_cfg_req.which, duty, period);
 		break;
 	case GB_PWM_TYPE_POLARITY:
 		payload_size = 0;
@@ -85,9 +77,8 @@ int pwm_handler(uint16_t cport_id, uint16_t hd_cport_id, void *rbuf,
 			libsoc_pwm_set_polarity(pwms[op_req->pwm_pol_req.which],
 						    op_req->pwm_pol_req.polarity);
 		}
-		gbsim_debug("AP -> Module %hhu CPort %hu PWM %d polarity (%s) request\n  ",
-			    module_id,
-			    cport_id, op_req->pwm_cfg_req.which,
+		gbsim_debug("PWM %d polarity (%s) request\n  ",
+			    op_req->pwm_cfg_req.which,
 			    op_req->pwm_pol_req.polarity ? "inverse" : "normal");
 		break;
 	case GB_PWM_TYPE_ENABLE:
@@ -95,39 +86,24 @@ int pwm_handler(uint16_t cport_id, uint16_t hd_cport_id, void *rbuf,
 		pwm_on[op_req->pwm_enb_req.which] = 1;
 		if (bbb_backend)
 			libsoc_pwm_set_enabled(pwms[op_req->pwm_enb_req.which], ENABLED);
-		gbsim_debug("AP -> Module %hhu CPort %hu PWM %d enable request\n  ",
-			    module_id, cport_id, op_req->pwm_enb_req.which);
+		gbsim_debug("PWM %d enable request\n  ",
+			    op_req->pwm_enb_req.which);
 		break;
 	case GB_PWM_TYPE_DISABLE:
 		payload_size = 0;
 		pwm_on[op_req->pwm_dis_req.which] = 0;
 		if (bbb_backend)
 			libsoc_pwm_set_enabled(pwms[op_req->pwm_dis_req.which], DISABLED);
-		gbsim_debug("AP -> Module %hhu CPort %hu PWM %d disable request\n  ",
-			    module_id, cport_id, op_req->pwm_dis_req.which);
+		gbsim_debug("PWM %d disable request\n  ",
+			    op_req->pwm_dis_req.which);
 		break;
 	default:
 		gbsim_error("pwm operation type %02x not supported\n", oph->type);
 		return -EINVAL;
 	}
 
-	/* Fill in the response header */
 	message_size = sizeof(struct op_header) + payload_size;
-	op_rsp->header.size = htole16(message_size);
-	op_rsp->header.id = oph->id;
-	op_rsp->header.type = OP_RESPONSE | oph->type;
-	op_rsp->header.result = result;
-	/* Store the cport id in the header pad bytes */
-	op_rsp->header.pad[0] = hd_cport_id & 0xff;
-	op_rsp->header.pad[1] = (hd_cport_id >> 8) & 0xff;
-
-	if (verbose)
-		gbsim_dump(op_rsp, message_size);
-	nbytes = write(to_ap, op_rsp, message_size);
-	if (nbytes < 0)
-		return nbytes;
-
-	return 0;
+	return send_response(op_rsp, hd_cport_id, message_size, oph, result);
 }
 
 char *pwm_get_operation(uint8_t type)
