c18b5bbc22b9ee96b45cf23fcac1042fde9f1262
Viresh Kumar <viresh.kumar@linaro.org>
2015-07-24 12:22:19 +0530

    gbsim: control: Start using send_response()
    Lets start using send_response() for control protocol drivers. This gets
    rid of lots of redundant code.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- control.c
+++ control.c
@@ -27,71 +27,37 @@ int control_handler(uint16_t cport_id, uint16_t hd_cport_id, void *rbuf,
 	struct op_msg *op_rsp = tbuf;
 	struct op_header *oph = &op_req->header;
 	uint16_t message_size = sizeof(*oph);
-	uint8_t module_id = cport_to_module_id(cport_id);
 	size_t payload_size;
-	ssize_t nbytes;
 
 	switch (oph->type) {
 	case GB_CONTROL_TYPE_PROTOCOL_VERSION:
 		payload_size = sizeof(op_rsp->pv_rsp);
 		op_rsp->pv_rsp.major = GB_CONTROL_VERSION_MAJOR;
 		op_rsp->pv_rsp.minor = GB_CONTROL_VERSION_MINOR;
-
-		gbsim_debug("Module %hhu -> AP CPort %hu CONTROL protocol version response\n  ",
-			    module_id, cport_id);
 		break;
 	case GB_CONTROL_TYPE_GET_MANIFEST_SIZE:
 		payload_size = sizeof(op_rsp->control_msize_rsp);
 		op_rsp->control_msize_rsp.size = htole16(info.manifest_size);
-
-		gbsim_debug("Module %hhu -> AP CPort %hu CONTROL manifest size response\n",
-			   module_id, cport_id);
 		break;
 	case GB_CONTROL_TYPE_GET_MANIFEST:
 		payload_size = info.manifest_size;
 		memcpy(&op_rsp->control_manifest_rsp.data, info.manifest,
 		       payload_size);
-
-		gbsim_debug("Module %hhu -> AP CPort %hu CONTROL get manifest response\n",
-			   module_id, cport_id);
 		break;
 	case GB_CONTROL_TYPE_CONNECTED:
 		payload_size = 0;
-
-		gbsim_debug("Module %hhu -> AP CPort %hu CONTROL connected response\n",
-			   module_id, cport_id);
 		break;
 	case GB_CONTROL_TYPE_DISCONNECTED:
 		payload_size = 0;
-
-		gbsim_debug("Module %hhu -> AP CPort %hu CONTROL dis-connected response\n",
-			   module_id, cport_id);
 		break;
 	default:
 		gbsim_error("control operation type %02x not supported\n", oph->type);
 		return -EINVAL;
 	}
 
-	/* Fill in the response header */
 	message_size += payload_size;
-	op_rsp->header.size = htole16(message_size);
-	op_rsp->header.id = oph->id;
-	op_rsp->header.type = OP_RESPONSE | oph->type;
-	op_rsp->header.result = PROTOCOL_STATUS_SUCCESS;
-
-	/* Store the cport id in the header pad bytes */
-	op_rsp->header.pad[0] = hd_cport_id & 0xff;
-	op_rsp->header.pad[1] = (hd_cport_id >> 8) & 0xff;
-
-	/* Send the response to the AP */
-	if (verbose)
-		gbsim_dump(op_rsp, message_size);
-
-	nbytes = write(to_ap, op_rsp, message_size);
-	if (nbytes < 0)
-		return nbytes;
-
-	return 0;
+	return send_response(op_rsp, hd_cport_id, message_size, oph,
+			     PROTOCOL_STATUS_SUCCESS);
 }
 
 char *control_get_operation(uint8_t type)
