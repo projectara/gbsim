d3b6fe42b6a9e5117f83cbd0f6b5bfdaf3abc355
Rui Miguel Silva <rui.silva@linaro.org>
2016-03-29 17:41:54 +0100

    gpio: add test case for irq_event
    This add a new test scenario for the irq mask event and mask handling.
    If a even gpio is exported and it's direction is input and with a edge
    type, the next odd gpio will be connected to it (if it is configured as
    output) and setting the value in the odd gpio will trigger an event to
    be send to greybus driver to the even gpio.
    This try to mimic a hw test case.
    
    Signed-off-by: Rui Miguel Silva <rui.silva@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- gpio.c
+++ gpio.c
@@ -32,6 +32,46 @@ struct gb_gpio {
 static struct gb_gpio gb_gpios[6];
 static gpio *gpios[6];
 
+static int gb_gpio_set_value(uint8_t which, uint8_t value)
+{
+	uint8_t which_con;
+
+	gb_gpios[which].value = value;
+
+	/*
+	 * check to see if it is even, we do nothig. If it is odd, we simulate
+	 * that the gpio is loopback to the even one before it. Of course, if
+	 * activated and if it is set as input
+	 */
+	if (!(which % 2))
+		return 0;
+
+	which_con = which - 1;
+
+	if (!gb_gpios[which_con].activated || gb_gpios[which_con].direction != 1)
+		return 0;
+
+	if (gb_gpios[which_con].value == value)
+		return 0;
+
+	gb_gpios[which_con].value = value;
+
+	 /* check to see if we need to send an irq event */
+	if (!gb_gpios[which_con].irq_type || !gb_gpios[which_con].irq_unmasked)
+		return 0;
+
+	if (gb_gpios[which_con].irq_type == GB_GPIO_IRQ_TYPE_EDGE_RISING &&
+	    value)
+		return 1;
+	else if (gb_gpios[which_con].irq_type == GB_GPIO_IRQ_TYPE_EDGE_FALLING &&
+		 !value)
+		return 1;
+	else if (gb_gpios[which_con].irq_type == GB_GPIO_IRQ_TYPE_EDGE_BOTH)
+		return 1;
+
+	return 0;
+}
+
 int gpio_handler(struct gbsim_connection *connection, void *rbuf,
 		 size_t rsize, void *tbuf, size_t tsize)
 {
@@ -42,7 +82,8 @@ int gpio_handler(struct gbsim_connection *connection, void *rbuf,
 	ssize_t nbytes;
 	uint16_t message_size;
 	uint16_t hd_cport_id = connection->hd_cport_id;
-	uint8_t which;
+	uint8_t which = 0;
+	int send_event = 0;
 
 	op_rsp = (struct op_msg *)tbuf;
 	oph = (struct gb_operation_msg_hdr *)&op_req->header;
@@ -115,7 +156,7 @@ int gpio_handler(struct gbsim_connection *connection, void *rbuf,
 		if (bbb_backend)
 			libsoc_gpio_set_level(gpios[which], op_req->gpio_set_val_req.value);
 		else
-			gb_gpios[which].value = op_req->gpio_set_val_req.value;
+			send_event = gb_gpio_set_value(which, op_req->gpio_set_val_req.value);
 		break;
 	case GB_GPIO_TYPE_SET_DEBOUNCE:
 		payload_size = 0;
@@ -152,11 +193,16 @@ int gpio_handler(struct gbsim_connection *connection, void *rbuf,
 
 #define TEST_HACK
 #ifdef TEST_HACK
-	/* Test GPIO interrupts by sending one when they become unmasked */
-	if (oph->type == GB_GPIO_TYPE_IRQ_UNMASK) {
+	/*
+	 * Test GPIO interrupts by sending one when they become unmasked, or
+	 * when set value trigger one
+	 */
+	if (send_event) {
 		payload_size = sizeof(struct gb_gpio_irq_event_request);
-		op_req->gpio_irq_event_req.which = 1;	/* XXX HACK */
+		op_req->gpio_irq_event_req.which = which - 1;
 
+		/* mask the irq to mimic fw action on event send */
+		gb_gpios[which - 1].irq_unmasked = 0;
 		message_size = sizeof(struct gb_operation_msg_hdr) + payload_size;
 		return send_request(hd_cport_id, op_req, message_size, 0,
 				    GB_GPIO_TYPE_IRQ_EVENT);
